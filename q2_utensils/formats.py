12df91e70c8feee09a18fbed853097b73d2d69e6
Keegan Evans <keegan.evans@gmail.com>
2021-10-04 14:47:58 -0700

    TEST: added testing for format validate

--- q2_utensils/formats.py
+++ q2_utensils/formats.py
@@ -2,8 +2,19 @@ import qiime2.plugin.model as Model
 
 from .plugin_setup import plugin
 
+from qiime2.plugin import ValidationError
+
 class UtensilsFormat(Model.TextFileFormat):
-    pass
+    def _validate_(self, level):
+        header_labels = 'utensil\taction\tmaterials\n'
+
+        with self.open() as fh:
+            for i, line in enumerate(fh, 0):
+                if i == 0:
+                    if line != header_labels:
+                        raise ValidationError("No header present or header"
+                                " does not contain correct fields:\nexpected:"
+                                " %s\nobserved: %s" % (header_labels, line))
 
 UtensilsDirectoryFormat = Model.SingleFileDirectoryFormat(
                           'UtensilsDirectoryFormat', 'utensil.tsv', UtensilsFormat)
--- q2_utensils/testing/test_format.py
+++ q2_utensils/testing/test_format.py
@@ -1,3 +1,4 @@
+import os.path
 from qiime2.plugin.testing import TestPluginBase
 import q2_utensils
 
@@ -7,4 +8,16 @@ class TestUtensilFormats(TestPluginBase):
     package = 'q2_utensils.testing'
 
     def test_format_registration(self):
-        self.assertTrue()
+        self.assertTrue(self)
+
+    def test_format_validate(self):
+        file_name = 'mixed_type_validator.tsv'
+
+        file_path = self.get_data_path(file_name)
+
+        format = UtensilsFormat(file_path, mode='r')
+
+        format.validate()
+
+    def test_format_validate_negative(self):
+        pass
